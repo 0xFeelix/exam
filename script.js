c9e016ff061c4ab9110b71af83f2acdab44a15ca
0xFeelix <mahammalaxov@gmail.com>
2021-11-18 13:03:31 +0200

    commit

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ function fn1()
             var result = document.getElementById("resultat");
 //MAHAM
             if (str == '001')       {
-                result.innerHTML = '<a href="/neticeler/maham.html" id="maham"></a>';
+                result.innerHTML = '<a href="/neticeler/maham.html" id="maham" target="_blank"></a>';
                 document.getElementById("maham").click()
             }
 //ALEX
             else if (str == '002')       {
-                result.innerHTML = '<a href="/neticeler/alex.html" id="alex"></a>';
+                result.innerHTML = '<a href="/neticeler/alex.html" id="alex" target="_blank"></a>';
                 document.getElementById("alex").click()
             } 
 //ROVSEN
             else if (str == '003')       {
-                result.innerHTML = '<a href="/neticeler/rovsen.html" id="rovsen"></a>';
+                result.innerHTML = '<a href="/neticeler/rovsen.html" id="rovsen" target="_blank"></a>';
                 document.getElementById("rovsen").click()
             } 
 //MARTIN
             else if (str == '004')       {
-                result.innerHTML = '<a href="/neticeler/martin.html" id="martin"></a>';
+                result.innerHTML = '<a href="/neticeler/martin.html" id="martin" target="_blank"></a>';
                 document.getElementById("martin").click()
             } 
 
